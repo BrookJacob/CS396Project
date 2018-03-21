8c2c8a080524580f05ec6b74897fb60d04ec03fe
BrookJacob <brookjacob@gmail.com>
2018-03-21 14:09:12 -0700

    PHP testing

--- php/search.php
+++ php/search.php
@@ -7,6 +7,14 @@
      );
      //Establishes the connection
      $conn = sqlsrv_connect($serverName, $connectionOptions);
-     if($conn)
-         echo "Connected!"
+     $search = $_POST["search"];
+     $tsql = "SELECT b.Title FROM books b WHERE b.Title LIKE '%" . $search . "%'";
+     $getResults = sqlserv_query($conn, $tsql);
+     echo ("Reading data from table" . PHP_EOL);
+     if ($getResults == FALSE)
+        echo (sqlsrv_errors());
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+        echo ($row['Title']. " " . PHP_EOL);
+    }
+    sqlsrv_free_stmt($getResults);
 ?>
