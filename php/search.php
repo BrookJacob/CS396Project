7ca4e30986b26cb442ccacf0657badcee2f6c0da
BrookJacob <brookjacob@gmail.com>
2018-03-21 13:52:02 -0700

    PHP testing

--- php/search.php
+++ php/search.php
@@ -1,3 +1,12 @@
 <?php
-    
+     $serverName = "librarybooks.database.windows.net";
+     $connectionOptions = array(
+         "Database" => "librarybooks",
+         "Uid" => "LBADMIN",
+         "PWD" => "Pirate88"
+     );
+     //Establishes the connection
+     $conn = sqlsrv_connect($serverName, $connectionOptions);
+     if($conn)
+         echo "Connected!"
 ?>
