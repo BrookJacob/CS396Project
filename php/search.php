7650ab55992504912e18f388db3bb833e550b528
BrookJacob <brookjacob@gmail.com>
2018-03-21 18:01:10 -0700

    Changed search.php to state if the connection was established

--- php/search.php
+++ php/search.php
@@ -7,13 +7,10 @@
      );
     //Establishes the connection
     $conn = sqlsrv_connect($serverName, $connectionOptions);
-    $tsql= "SELECT * FROM books";
-    $getResults= sqlsrv_query($conn, $tsql);
-    echo ("Reading data from table" . PHP_EOL);
-    if ($getResults == FALSE)
-        echo (sqlsrv_errors());
-    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-        echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
+    if( $conn ) {
+        echo "Connection established. <br />";
+    } else {
+        echo "Connection could not be established. <br />";
+        die( print_r( sqlsrv_errors(), true));
     }
-    sqlsrv_free_stmt($getResults);
 ?>
