6e1995a66d39b8da1b1c4c54a05ac973328b588d
BrookJacob <brookjacob@gmail.com>
2018-03-21 18:07:31 -0700

    php

--- php/search.php
+++ php/search.php
@@ -13,4 +13,10 @@
         echo "Connection could not be established. <br />";
         die( print_r( sqlsrv_errors(), true));
     }
+    $sql = "SELECT * FROM books";
+    $stmt = sqlsrv_query( $conn, $sql);
+    if( $stmt === false) {
+        die(print_r( sqlsrv_errors(), true));
+    }
+    sqlsrv_close( $conn );
 ?>
