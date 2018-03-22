f97230033bacee1923477c1060da9ccc0e903dc8
BrookJacob <brookjacob@gmail.com>
2018-03-21 17:46:03 -0700

    Rewrote search.php

--- php/search.php
+++ php/search.php
@@ -1,20 +1,19 @@
 <?php
-     $serverName = "librarybooks.database.windows.net";
-     $connectionOptions = array(
-         "Database" => "librarybooks",
-         "Uid" => "LBADMIN",
-         "PWD" => "Pirate88"
+    $serverName = "librarybooks.database.windows.net";
+    $connectionOptions = array(
+        "Database" => "librarybooks",
+        "Uid" => "LBADMIN",
+        "PWD" => "Pirate88"
      );
-     //Establishes the connection
-     $conn = sqlsrv_connect($serverName, $connectionOptions);
-     $search = $_POST["search"];
-     $tsql = "SELECT title FROM books WHERE title like '%$search%'";
-     $getResults = sqlserv_query($conn, $tsql);
-     echo ("Reading data from table" . PHP_EOL);
-     if ($getResults == FALSE)
-        echo ("help");
+    //Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    $tsql= "SELECT * FROM books";
+    $getResults= sqlsrv_query($conn, $tsql);
+    echo ("Reading data from table" . PHP_EOL);
+    if ($getResults == FALSE)
+        echo (sqlsrv_errors());
     while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-        echo ($row['title']. " " . PHP_EOL);
+        echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
     }
     sqlsrv_free_stmt($getResults);
 ?>
