42b0511e63ecf12ddb4744302f1d061fe6896d90
brookjacob <brookjacob@gmail.com>
2018-04-04 20:57:39 -0700

    php

--- php/search.php
+++ php/search.php
@@ -1,10 +1,3 @@
-<html>
-	<head>
-	</head>
-	<body>
-	hello
-	</body>
-</html>
 <?php
 	$file = fopen("../../../data/connection.txt", "r") or die("Unable to open file!");
 	$psswrd = fread($file, 8);
@@ -22,7 +15,7 @@
         die( print_r( sqlsrv_errors(), true));
     }
     $search = $_REQUEST['main-search-bar'];
-	$sql = "SELECT b.ISBN, b.title, b.author, g.genreName FROM books AS b, genre as G WHERE title LIKE '%". $search . "%' AND b.genreID = g.genreID";
+	$sql = "SELECT b.ISBN, b.title, b.author, g.genreName FROM books AS b, genre as G WHERE b.title LIKE '%". $search . "%' OR b.author LIKE '%". $search . "%' OR b.ISBN LIKE '%". $search . "%' OR g.genreName LIKE '%". $search . "%' AND b.genreID = g.genreID";
     $stmt = sqlsrv_query( $conn, $sql);
 	while ( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
 		echo $row['ISBN'].", ".$row['title'].", ".$row['author'].", ".$row['genreName']."<br />";
