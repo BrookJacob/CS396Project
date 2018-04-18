5e2c3d091c8da0dfde39175479e27cda31a217cb
BrookJacob <brookjacob@gmail.com>
2018-04-18 15:58:03 -0700

    php

--- php/search.php
+++ php/search.php
@@ -26,7 +26,7 @@
     //Establishes the connection
     $conn = sqlsrv_connect($serverName, $connectionOptions);
     if( $conn ) {
-        echo "Connection established. <br />";
+        echo "<p class=Connection established. <br />";
     } else {
         echo "Connection could not be established. <br />";
         die( print_r( sqlsrv_errors(), true));
