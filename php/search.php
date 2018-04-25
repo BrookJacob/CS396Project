48bed81d86a428bc8cd7da3fb70095481f7ce09e
BrookJacob <brookjacob@gmail.com>
2018-04-25 15:19:04 -0700

    php

--- php/search.php
+++ php/search.php
@@ -19,8 +19,8 @@
 <?php
 	require("common.php");
     $search = $_REQUEST['main-search-bar'];
-	$sql = "SELECT b.title, b.author, g.genreName FROM books AS b, genres as g WHERE b.title LIKE '%?%' OR b.author LIKE '%?%' OR b.ISBN10 = '?' OR b.ISBN13 = '?' OR g.genreName LIKE '%?%' AND b.genreID = g.genreID";
-    $stmt = sqlsrv_query( $conn, $sql);
+	$sql = "SELECT b.title, b.author, g.genreName FROM books AS b, genres as g WHERE b.title LIKE '%". $search . "%' OR b.author LIKE '%" . $search . "%' OR b.ISBN13 = '" . $search . "' OR g.genreName LIKE '%" . $search . "%' AND b.genreID = g.genreID";
+    $stmt = sqlsrv_query( $conn, $sql );
 	while ( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
 		echo "<li class='search-result'>".$row['title'].", ".$row['author'].", ".$row['genreName']."</li>";
 	}
