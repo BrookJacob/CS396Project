354e1c8fd5c7297f918a60d9d18dcb9bdc072fd5
BrookJacob <brookjacob@gmail.com>
2018-04-25 15:49:43 -0700

    php

--- php/search.php
+++ php/search.php
@@ -7,9 +7,9 @@
     <body class="splash">
         <div class="menu-bar">
             <ul class="menu-buttons">
-            <li class="menu-button"><a class="menu-button-link" href="index.html">library books</a></li>
-                <li class="menu-button"><a class="menu-button-link" href="index.html#about">about</a></li>
-                <li class="menu-button"><a class="menu-button-link" href="index.html#feedback">feedback</a></li>
+            <li class="menu-button"><a class="menu-button-link" href="../index.html">library books</a></li>
+                <li class="menu-button"><a class="menu-button-link" href="../index.html#about">about</a></li>
+                <li class="menu-button"><a class="menu-button-link" href="../index.html#feedback">feedback</a></li>
                 <li class="menu-button menu-right"><a class="menu-button-link" href="account.html">sign in</a></li>
                 <li class="menu-button menu-right"><a class="menu-button-link" href="php/register.php">sign up</a></li>
                 <li class="cheat"></li>
