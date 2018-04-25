05b6f66c99e80bfd3cba4e75a091636e2361d4ed
BrookJacob <brookjacob@gmail.com>
2018-04-25 14:53:33 -0700

    php

--- php/search.php
+++ php/search.php
@@ -4,24 +4,28 @@
     <link href="../css/main.css" type="text/css" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet">
 </head>
-    <body class="splash">
-        <div class="menu-bar">
-            <ul class="menu-buttons">
-                <li class="menu-button"><a class="menu-button-link" href="../index.html">library books</a></li>
-                <li class="menu-button"><a class="menu-button-link" href="../index.html#about">about</a></li>
-                <li class="menu-button"><a class="menu-button-link" href="../index.html#feedback">feedback</a></li>
-                <li class="menu-button sign-in"><a class="menu-button-link" href="../account.html">sign in</a></li>
-                <li class="menu-button sign-up"><a class="menu-button-link" href="../register.php">sign up</a></li>
-            </ul>
-        </div>
-        <div class="backsplash">
-            <ul class="search-results">
+<body class="splash">
+    <div class="menu-bar">
+        <ul class="menu-buttons">
+            <li class="menu-button"><a class="menu-button-link" href="../index.html">library books</a></li>
+            <li class="menu-button"><a class="menu-button-link" href="../index.html#about">about</a></li>
+            <li class="menu-button"><a class="menu-button-link" href="../index.html#feedback">feedback</a></li>
+            <li class="menu-button sign-in"><a class="menu-button-link" href="../account.html">sign in</a></li>
+            <li class="menu-button sign-up"><a class="menu-button-link" href="../register.php">sign up</a></li>
+        </ul>
+    </div>
+    <div class="backsplash">
+        <ul class="search-results">
 <?php
 	require("common.php");
     $search = $_REQUEST['main-search-bar'];
+    echo $search;
     $sql = "SELECT b.title, b.author, g.genreName FROM books AS b, genres as g WHERE b.title LIKE '% ? %' OR b.author LIKE '% ? %' OR b.ISBN13 = ' ? ' OR b.ISBN10 = ' ? ' OR g.genreName LIKE '% ? %' AND b.genreID = g.genreID";
+    echo $sql;
     $params = array($_POST['main-search-bar']);
+    echo $params[0];
     $stmt = sqlsrv_query( $conn, $sql, $params);
+    echo $stmt;
     if( $stmt === false){
         die(print_r( sqlsrv_errors(), true));
     }
@@ -31,7 +35,7 @@
     
     sqlsrv_close( $conn );
 ?>
-            </ul>
-        </div>
+        </ul>
+    </div>
 </body>
 </html>
