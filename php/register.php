8569c768ccf62ebb302456b325124b190bf8473e
brookjacob <brookjacob@gmail.com>
2018-04-25 19:18:03 -0700

    php

--- php/register.php
+++ php/register.php
@@ -67,14 +67,16 @@
         }
         $firstname = $_POST['first-name'];
         $lastname = $_POST['last-name'];
-        $salt = dechex(mt_rand(0, 2147483647)) . dechex(mt_rand(0, 2147483647));
+        /*$salt = dechex(mt_rand(0, 2147483647)) . dechex(mt_rand(0, 2147483647));
         $userPassword = hash('sha256', $_POST['password'] . $salt);
-        for( $round = 0; $round < 65536; $round++){
+		*/
+		$userPassword = $_POST['password'];
+        /*for( $round = 0; $round < 65536; $round++){
             $userPassword = hash('sha256', $userPassword . $salt);
-        }
+        }*/
         $sql = "INSERT INTO users (firstName, lastName, username, email, userPassword, salt)
                 VALUES (?, ?, ?, ?, ?, ?)";
-        $params = array( &$firstname, &$lastname, &$username, &$email, &$userPassword, &$salt);
+        $params = array( &$firstname, &$lastname, &$username, &$email, &$userPassword);
         $stmt = sqlsrv_query( $conn, $sql, $params);
         if ( $stmt === true ){
             die("could not execute query.");
