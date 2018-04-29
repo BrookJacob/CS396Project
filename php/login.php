1c95646610b9b29dcb254c4d6209d1777b9b1dd6
BrookJacob <brookjacob@gmail.com>
2018-04-28 23:04:53 -0700

    php

--- php/login.php
+++ php/login.php
@@ -46,7 +46,7 @@
         if(password_verify( $password, $row['userPassword'] )){
             $login = true;
         }
-        if($login = true){
+        if($login == true){
             
             $_SESSION['user'] = $stmt;
 
