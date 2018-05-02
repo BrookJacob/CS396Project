be5aa2ce6a58894382628e7b25da0185fa251d4a
BrookJacob <brookjacob@gmail.com>
2018-05-02 14:51:23 -0700

    php

--- php/login.php
+++ php/login.php
@@ -31,7 +31,7 @@
         $username = trim($_POST['username']);
         $password = trim($_POST['password']);
 
-        $sql = "SELECT userID, firstName, lastName, email, userPassword FROM users WHERE username = '?'";
+        $sql = "SELECT userID, firstName, lastName, email, userPassword FROM users WHERE username = '".$username."'";
         $params = array( &$username );
         $stmt = sqlsrv_query( $conn, $sql, $params );
         echo sqlsrv_errors();
