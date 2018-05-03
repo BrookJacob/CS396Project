4c5ed20c199519db2fa25a62fa199d7bf65b10ca
brookjacob <brookjacob@gmail.com>
2018-05-02 20:41:54 -0700

    php

--- php/result.php
+++ php/result.php
@@ -1,15 +1,15 @@
 <html>
     <head>
         <title>library books</title>
-        <link href="css/main.css" type="text/css" rel="stylesheet" />
+        <link href="../css/main.css" type="text/css" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet">
     </head>
     <body class="splash">
         <div class="menu-bar">
             <ul class="menu-buttons">
-                <li class="menu-button"><a class="menu-button-link" href="index.html">library books</a></li>
-                <li class="menu-button"><a class="menu-button-link" href="index.html#about">about</a></li>
-                <li class="menu-button"><a class="menu-button-link" href="index.html#feedback">feedback</a></li>
+                <li class="menu-button"><a class="menu-button-link" href="../index.html">library books</a></li>
+                <li class="menu-button"><a class="menu-button-link" href="../index.html#about">about</a></li>
+                <li class="menu-button"><a class="menu-button-link" href="../index.html#feedback">feedback</a></li>
                 <li class="menu-button menu-right"><a class="menu-button-link" href="php/login.php">sign in</a></li>
                 <li class="menu-button menu-right"><a class="menu-button-link" href="php/register.php">sign up</a></li>
                 <li class="cheat"></li>
