54b9f02f91cfd477e14f669ffbf4ad3f6a67c6ae
brookjacob <brookjacob@gmail.com>
2018-05-02 21:05:53 -0700

    php

--- php/result.php
+++ php/result.php
@@ -21,7 +21,7 @@
 
                 $ISBN13 = $_GET['ISBN13'];
                 echo $ISBN13;
-                $sql = "SELECT b.ISBN10, b.ISBN13, b.author, b.title, g.genreName FROM books AS b, genres as g WHERE b.ISBN13 = '?' AND g.genreID = b.genreID";
+                $sql = "SELECT b.ISBN10, b.ISBN13, b.author, b.title, g.genreName FROM books AS b, genres as g WHERE b.ISBN13 = '".$ISBN13."' AND g.genreID = b.genreID";
                 $params = array( &$ISBN13 );
 
                 $stmt = sqlsrv_query( $conn, $sql, $params);
