c176a86665ed882882539004a495f7dd8f321a92
BrookJacob <brookjacob@gmail.com>
2018-05-04 14:14:55 -0700

    php

--- php/liveResults.php
+++ php/liveResults.php
@@ -10,5 +10,15 @@
             echo sqlsrv_errors();
             die( print_r( sqlsrv_errors(), true) );
         }
+        echo '<ul>';
+        while ($row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC )){
+            ?>
+            <li onclick='fill("<?php echo $row['title']; ?>")'>
+            <a>
+            <?php echo $row['title']; ?>
+        </li></a>
+        <?php
+        }
     }
 ?>
+</ul>
