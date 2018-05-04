bc81e2e53e17161d907f27b6be15aafe1d3212fb
BrookJacob <brookjacob@gmail.com>
2018-05-04 14:13:45 -0700

    php

--- php/liveResults.php
+++ php/liveResults.php
@@ -1,13 +1,14 @@
 <?php
     require("common.php");
-    $q = $_GET['q'];
-    $sql = "SELECT b.title, b.author, g.genreName FROM books AS B, genres AS g WHERE b.title = '%?%' OR b.author = '%?%' OR g.genreName = '%?%' OR b.ISBN10 = '?' OR b.ISBN13 = '?' AND b.genreID = g.genreID";
-    $params = array( &$q );
+    if(isset($_POST['main-search-bar'])) {
+        $q = $_POST['main-search-bar'];
+        $sql = "SELECT b.title, b.author, g.genreName FROM books AS b, genres AS g WHERE b.title LIKE '%?%' OR b.author LIKE '%?%' OR g.genreName LIKE '%?%' OR ISBN10 = '?' OR ISBN13 = '?' AND g.genreID = b.genreID";
+        $params = array( &$q );
+        $stmt = sqlsrv_query( $conn, $sql, $params);
 
-    $stmt = sqlsrv_query( $conn, $sql, $params);
-    if( $stmt == false){
-        echo sqlsrv_errors();
-        die( print_r( sqlsrv_errors(), true) );
+        if ($stmt == false){
+            echo sqlsrv_errors();
+            die( print_r( sqlsrv_errors(), true) );
+        }
     }
-    echo 'hello';
 ?>
