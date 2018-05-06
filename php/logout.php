63866f6799ec2c4da7582cee3c533e239b4314a6
brookjacob <brookjacob@gmail.com>
2018-05-06 12:49:44 -0700

    php

--- php/logout.php
+++ php/logout.php
@@ -1,12 +1,8 @@
 <?php
     require("common.php");
-    if(empty($_SESSION['user'])){
-        header("Location: login.php");
-        die("Redirecting to login.php");
-    } else {
-        unset($_SESSION['user'])
-        header("Location: ../index.php");
-        die("Redirecting to ../index.php");
-    }
+   
+    unset($_SESSION['user'])
+    header("Location: ../index.php");
+    die("Redirecting to ../index.php");
 
 ?>
