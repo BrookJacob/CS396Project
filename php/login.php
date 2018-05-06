2aab2079a26f7dafa67403c472ed3b3f2ec0fd82
brookjacob <brookjacob@gmail.com>
2018-05-06 11:35:50 -0700

    php

--- php/login.php
+++ php/login.php
@@ -39,9 +39,8 @@
             die( print_r( sqlsrv_errors(), true) );
         }
         $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC);
-        if($row){
-            echo "retreived row";
-        }
+        echo $row['username'];
+        
         $hash = $row['userPassword'];
         
         if(password_verify( $password, $hash )){
