85d38886558ca4a831c6c76320979a17788ad9dc
brookjacob <brookjacob@gmail.com>
2018-05-06 11:48:28 -0700

    php

--- php/result.php
+++ php/result.php
@@ -16,7 +16,7 @@
                 <li class="cheat"></li>
         </div>
         <div class="result-search-bar">
-            <form class="main-search-bar" name="search" action="php/search.php?go" method="post">
+            <form class="main-search-bar" name="search" action="search.php?go" method="post">
                 <input class="main-search-bar" type="text" name="main-search-bar" placeholder="isbn, title, author, genre" autocomplete="off">
             </form>
             <div class="live-results"></div>	
