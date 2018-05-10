a4f870a43fe9d9bde63a734144ca6119f75bf507
brookjacob <brookjacob@gmail.com>
2018-05-09 20:29:13 -0700

    php

--- php/account.php
+++ php/account.php
@@ -38,7 +38,7 @@
                 <input class="account-input" type="text" placeholder="email" name="email"><br />
                 <input class="account-input" type="text" placeholder="confirm email" name="confirm-email"><br />
                 <p>change your password</p>
-                <input class="account-input" type="password" placeholder="old password" name="old-password"><br />
+                <input class="account-input" type="password" placeholder="current password" name="old-password"><br />
                 <input class="account-input" type="password" placeholder="new password" name="new-password"><br />
                 <input class="account-input" type="password" placeholder="confirm new password" name="confirm-password"><br />
                 <input class="account-input" type="submit" value="update"><br />
