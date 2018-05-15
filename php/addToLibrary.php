56672a2a65ee25c39fa11f6f0876e5e3ed7e686b
BrookJacob <brookjacob@gmail.com>
2018-05-15 16:00:15 -0700

    everything

--- php/addToLibrary.php
+++ php/addToLibrary.php
@@ -5,7 +5,7 @@
 
     $userID = $_SESSION['user']['userID'];
     $ISBN13 = $_GET['q'];
-    $sql = "INSERT INTO library ( userID, ISBN13 ) VALUES ( ?, ? )";
+    $sql = "INSERT INTO userlibrary ( userID, ISBN13 ) VALUES ( ?, ? )";
     $params = array( &$userID, &$ISBN13 );
     $stmt = sqlsrv_query( $conn, $sql, $params );
     if( $stmt === false ){
