162c7abe278482a7747ad23c2893876ab32c8bc3
BrookJacob <brookjacob@gmail.com>
2018-05-15 16:10:13 -0700

    php

--- php/library.php
+++ php/library.php
@@ -43,6 +43,7 @@
         die("Redirecting to login.php");
     }
     $userID = $_SESSION['user']['userID'];
+    echo $userID;
     $sql = "SELECT b.ISBN10, b.ISBN13, b.title, b.author, g.genreName FROM books AS b, genres AS g, userlibrary AS l WHERE g.genreID = b.genreID AND l.ISBN13 = b.ISBN13 AND l.userID = ?";
     $params = array( &$userID );
     $stmt = sqlsdrv_query( $conn, $sql, $params );
